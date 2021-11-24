ba4636588b1af607a9f70f3441ee82f0970c5dd4
sadiaasim <seive14@gmail.com>
2021-11-25 00:45:34 +0100

    TDD: Check that the attempt exists in the English Dictionary

--- tests/test_game.py
+++ tests/test_game.py
@@ -26,3 +26,7 @@ class TestGame(unittest.TestCase):
         self.assertIs(new_game.is_valid('SANDWICH'), False)
         self.assertEqual(new_game.grid, list('KWEUEAKRZ'))
 
+    def test_unknown_word_is_invalid(self):
+      new_game = Game()
+      new_game.grid = list('KWIENFUQW')
+      self.assertIs(new_game.is_valid('FEUN'), False)
